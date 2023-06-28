3aba8ad608e3a69a2f96810d6556d78d367f052c
Chandana4518 <137889125+Chandana4518@users.noreply.github.com>
2023-06-28 21:12:01 +0530

    Add files via upload

---
+++ CONSTRUCTOR/Bacteria.java
@@ -0,0 +1,34 @@
+class Bacteria{
+	Bacteria()
+	{
+		System.out.println("Bacteria infected all the people...");
+	}
+}
+class BacteriaRunner{
+	public static void main(String[]args)
+	{
+		System.out.println("Runnig Main Method");
+		Bacteria bacteria=new Bacteria();
+		System.out.println("Bacteria causes diseases");
+		Bacteria tiny=new Bacteria();
+		System.out.println("Bacteria is harmful");
+		Bacteria organism=new Bacteria();
+		System.out.println("Chlorine is used to kill bacteria");
+		Bacteria water=new Bacteria();
+		System.out.println("This drug is effective for bacteria");
+		Bacteria effective=new Bacteria();
+		System.out.println("Bacteria is something invisible");
+		Bacteria drug=new Bacteria();
+		System.out.println("Bacteria are too small");
+		Bacteria harm=new Bacteria();
+		System.out.println("Bacteria initiates the infection");
+		Bacteria kill=new Bacteria();
+		System.out.println("Bacteria.....");
+		Bacteria small=new Bacteria();
+		System.out.println("Bacteria decomposes the milk");
+		Bacteria infection=new Bacteria();
+		System.out.println("Bacteria will not breed in alcohol");
+		Bacteria decompose=new Bacteria();
+		System.out.println("bacteria is single called organism");
+	}
+}
---
+++ CONSTRUCTOR/Park.java
@@ -0,0 +1,24 @@
+class Park{
+	Park()
+	{
+		System.out.println("The park was filled with people");
+	}
+}
+class ParkRunner{
+	public static void main(String[]args)
+	{
+		System.out.println("Main Method");
+		Park park=new Park();
+		Park picnic=new Park();
+		Park crossed=new Park();
+		Park people=new Park();
+		Park whole=new Park();
+		Park dash=new Park();
+		Park vehicle=new Park();
+		Park permit=new Park();
+		Park mountain=new Park();
+		Park attend=new Park();
+		Park allow=new Park();
+	}
+}
+		
