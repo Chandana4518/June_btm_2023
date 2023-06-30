e62dffe48098852e7ecd9724b5006137798670e9
Chandana4518 <chandanad.xworkz@gmail.com>
2023-06-30 13:57:36 +0530

    Test

---
+++ Test/Javatest.java
@@ -0,0 +1,163 @@
+1)Java And Javac?
+
+    Java and Javac both are JDK tools.Javac is used to compile the code and Javac is used to run the code
+java is object oriented programming language and platform independent.
+
+2)JDK,JRE,JVM?
+
+JDK-Java Development Kit
+In the JDK we can write the program and it strores in JDK bin folder.
+JRE-Java Runtime Environment
+JRE is used to execute the program
+JVM-Java Virtual Machine
+JVM is used to execute actual program
+
+3)Limitations of array?
+
+we have implicit and explicit choice in arrays
+Once the array size created it will not change again
+If we want to change we have to execute program again then it will allocate the size of the array.
+
+4)Class,Object,Signature of the Class?
+
+Class-Class is a blueprint of the object and class is non-primitive,datatype
+Every class should have the body of the program
+Object-Obeject is a Real time entity,whatever we are using the things those all are objects
+that objects are using for method declaration.
+Class signature?
+class Metro{
+System.out.println("Metro");//statement
+}
+Metro represents class name and {}these are represents body of the program 
+
+5)Array?
+
+Array is a Structured container which holds the data in sequence manner
+Array contains index boxes
+in array we can use iteration process
+
+6).javafile and .classfile
+
+.javafile- .javafile is readable and understandable for developers and users.It is in english language
+.classfile-.classfile is only understandable for system because it is in byte code means it is in zeros and ones.
+
+7)Length of the array:byte[]data={12,34,9,0,-62,88}
+
+Length of the array is:len=length.data;
+len=6
+
+8)Method signature
+
+class classname{
+	public static void main(String[]args)//Main method declaration
+	{
+		
+	}
+}
+public is acess specifier
+static is non-acess specifier 
+void is return type
+main method name
+(String[]args) is parameters
+
+9)java?
+
+java is a object-oriented programming language
+It is platform independent
+Once the code compiled it will generate bytecode if you want to run code on another platform the jvm will give bytecode no need to complile again.
+
+10)Variable?
+
+Variable is used to store the memory of the data
+we can also call it ass refference variable.we cangive anyname to the refference variable but stating letter should be small because it is standard way.
+
+
+11)Class variables?
+
+Class variables means whatever we write as refference variables all are class variables.
+
+12)Datatypes?
+
+Datatype is used to know which data type we have declared
+datatypes are two types 1.Primitive  2.Non-primitive
+Primitive datatypes       
+int                       
+float
+double
+boolean
+short
+byte
+Non-primitive datatypes
+String
+char
+
+13)Java features?
+
+Object oriented programming language
+Platform independent
+Have JVM
+Security
+Web development
+Applications
+
+14)Types of array?
+
+15)Array index out of the box exception?
+
+Array index out of the box is one of the Syntax error
+This error occures when we give the index number out of length while we are retreiving the array.
+For example,the length is 10 but we gave the index number 12 then we will get this array out of the box exception.
+
+16)System.out.println();
+
+It is a Statement
+System is pre defined class
+. It is dot operator which is used acess class members
+out is static variables
+println is static method
+() In this parameters we can give any message we want.
+
+17)double equal operator
+
+It is used to check whether the two variables hold the same value or not.
+a=2
+b=2
+then we can say a==b
+If the variable values are not equal that time we will get error if we use this double equal operator
+
+18)Use of . Operator?
+
+Dot Operator is used to acess the members of class
+
+19)base class of all classes?
+
+Base class for the all classes is where we have created the main method that is base class
+If in that program there is no main method then you will get error it will not compile.So,main method  class is base class for all classes. 
+class Trial{
+System.out.println("Message");
+}
+class Runner{
+public static void main(String[]args)//Base class for all the classes
+{
+System.out.println("Main Method");
+}
+}
+
+20)10 Java Keywords?
+
+1.static
+2.void
+3.String
+4.goto
+5.return
+6.private
+7.public
+8.packages
+9.float
+10.double
+
+
+
+
+
+
