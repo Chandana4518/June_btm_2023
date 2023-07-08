c8c8a85f5a5f4889299527c0ec01d83eb58593a1
Chandana4518 <chandanad.xworkz@gmail.com>
2023-07-08 13:21:02 +0530

    July 8th...Test

---
+++ Test-July 8th/Test.java
@@ -0,0 +1,96 @@
+1)False
+
+2)101,101
+
+3)True
+
+4)Static is a Keyword,and static variables declare within the class and 
+uses outside of the method which is starts with static
+
+5)class ReverseArray{
+	public static void main(String[]args)
+	{
+int a,b,c,d,e;
+int[] reverseArray={10,45,7,9,2};
+int len=reverseArray.length;
+system.out.println("Length :"+len);
+for(int index=len;index>45;index--)
+{
+	int name=reverseArray[index];
+System.out.println("Reverse Array :"+name);
+}
+}
+}
+6)class Descending{
+	public static void main(String[]args)
+	{
+		int a,b,c,d,e,f,g;
+		int[] descending={2,5,4,8,3,6,2};
+		int len=descending.length;
+		System.out.println(len);
+		for(int index=len;index>10;index--)
+		{
+			int name=descending[index];
+			System.out.println("Descending order :"+name);
+		}
+	}
+}
+		
+7)Fail
+
+8)this and this()
+this is a keyword which is used to access the instance variables 
+it is used to remove the confusion between instance variables and local variables.
+It Occurs when the local variable and instance variable same.
+this() It is used for Constructor or method chaining to call the instance variables from one constructor 
+method to another constructor with in the same class.
+
+9)
+class Swapping{
+	public static void main(String[]args)
+	{
+		int a=2;
+		int b=4;
+		int c;
+		c=a+b;
+		a=c-a;
+		b=c-b;
+		System.out.println("a :"+a);
+		System.out.println("b :"+b);
+		
+	}
+}
+
+10)
+1)class Pattern
+{
+	public static void main(String[]args)
+	{
+		Sytsem.out.println("&"+" "+"&");
+		System.out.println(" "+"&"+" ");
+		System.out.println("&"+" "+"&");
+	}
+}
+
+2)class Pattern1
+{
+	public static void main(String[]args)
+	{
+		System.out.println("%"+"%"+"%");
+		System.out.println("%"+"&"+"%");
+		System.out.println("%"+"%"+"%");
+	}
+}
+
+3)class Pattern2
+{
+	public static void main(String[]args)
+	{
+		System.out.println("%"+"%"+"%");
+		System.out.println("%"+" "+"%");
+		System.out.println("%"+"%"+"%");
+	}
+}
+		
+
+		
